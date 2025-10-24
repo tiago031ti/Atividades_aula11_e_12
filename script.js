c28b6480441e3f06aed9f4134b127d525af318cf
Tiago Mendes <tgcmendes80@gmail.com>
2025-10-24 20:30:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("ola mundo!");
+document.getElementById("titulo").innerHTML ="JS é incrivel";
+
+const buttonalert =document.getElementById("buttonalert");
+buttonalert.addEventListener('click', function() {
+    alert ("O botao foi clicado!!")
+});
---
+++ scriptt.js
@@ -0,0 +1,16 @@
+const nome = {
+    meunome: "Tiago Mendes"
+};
+console.log(nome);
+
+let idade = 18;
+idade = 19;
+console.log(idade);
+
+const produto = {
+    nome: "Arroz",
+    valor: 10
+};
+produto.valor = 15;
+
+console.log(produto);
